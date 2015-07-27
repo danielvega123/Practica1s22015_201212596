daa60846e435a2e7183ec579897a5592f80ea341
danielvega <danielvega@daniel>
2015-07-26 20:26:59 -0600

    practica

--- src/practica1s22015_201212596/Practica1s22015_201212596.java
+++ src/practica1s22015_201212596/Practica1s22015_201212596.java
@@ -15,8 +15,8 @@ public class Practica1s22015_201212596 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
+        PantPrincipal_201212596 pant = new PantPrincipal_201212596();
+       pant.show();
     }
     
 }
